00175fb7b67b7113dd42cb37081eacac88b5e0ac
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-06-08 22:48:12 +0800

    add match choices with TODOs

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 
-UNSET_OPTION = 0
 EXIT_OPTION = 6
+UNSET_OPTION = -1
 
 def clear_screen():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def display_menu():
-    print("-----Menu-----")
-    print("1. Option 1")
-    print("2. Option 2")
-    print("3. Option 3")
-    print("4. Option 4")
-    print("5. Option 5")
+    print("================================")
+    print("              Menu              ")
+    print("================================")
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    print("5. ")
     print("6. Exit")
+    print("================================")
 
 def get_user_choice():
     try:
@@ -21,6 +24,26 @@ def get_user_choice():
     except ValueError:
         return UNSET_OPTION
 
+def display_get_choice(choice):
+    match choice:
+        case 1:
+             #TO-DO(Raquem): call your module here
+            pass
+        case 2:
+            #TO-DO(Victorio): call your module here
+            pass
+        case 3:
+            #TO-DO(Capilitan): call your module here
+            pass
+        case 4:
+            #TO-DO(Niones): call your module here
+            pass
+        case 5:
+            #TO-DO(Villarta): call your module here
+            pass
+        case _:
+            print("Invalid choice. Try again.")
+            
 def main():
     while True:
         clear_screen()
@@ -32,9 +55,10 @@ def main():
             break
         elif 1 <= choice <= 5:
             print(f"You selected Option {choice}.")
+            display_get_choice(choice)
+            input("Press Enter to continue...")
         else:
             print("Invalid choice. Try again.")
-
-        input("Press Enter to continue...")
+            input("Press Enter to continue...")
 
 main()
