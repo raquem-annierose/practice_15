e2a6468b65ec101a57510f9f09c35ac7e5657be3
capilitan_mikee <mikee.capilitan@gmail.com>
2025-06-09 22:13:28 +0800

    add initial skeleton for StudentLifeManager class

---
+++ polytech/capilitan.py
@@ -0,0 +1,30 @@
+import os
+import random
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+class StudentLifeManager:
+    def __init__(self):
+        self.student_name = ""
+        self.student_school = ""
+        self.todo_list = []
+        self.quotes = []
+
+    def view_profile(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def view_tasks(self):
+        pass
+
+    def clear_tasks(self):
+        pass
+
+    def motivate_me(self):
+        pass
+
+    def menu(self):
+        pass
